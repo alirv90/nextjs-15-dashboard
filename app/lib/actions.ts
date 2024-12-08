6938ee6119276994b6ee997c2e1b932ca80cb1da
alirv90 <ali.rv90@gmail.com>
2024-12-08 16:18:19 +0700

    chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,14 +68,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice.");
-
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath("/dashboard/invoices");
-  } catch (error) {
-    return {
-      message: "Database Error: Failed to Delete Invoice.",
-    };
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
